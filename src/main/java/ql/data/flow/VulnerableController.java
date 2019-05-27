e427ad092f4b2bd1705097ca20455a6422119b4e
Cornelius <corni@localhost.localdomain>
2019-05-28 00:34:22 +0200

    Adjust examples, add comments describing the reason why it should (not) trigger an error.

--- src/main/java/ql/data/flow/VulnerableController.java
+++ src/main/java/ql/data/flow/VulnerableController.java
@@ -12,6 +12,10 @@ public class VulnerableController {
 		return Paths.get(input).toString();
 	}
 
+	public static int addTwo(int a) {
+		return a + 2;
+	}
+
 	public static String vulnIfX0(String input, int x) {
 		if (x == 0) {
 			return Paths.get(input).toString();
@@ -30,36 +34,57 @@ public class VulnerableController {
 
 	@GetMapping("/vulnerable")
 	public String vulnerable(@RequestParam(name = "name", required = false, defaultValue = "/etc/passwd") String path) {
-		// should be enough to trigger the taint analysis
-		return vuln(path);
+		// vulnerable code, triggering an error (checks that the basic setup is working)
+		return vuln(path) + vulnIfX0(path, 0) + vulnIfFalse(path, false);
 	}
 
 	@GetMapping("/rangeAnalysis")
 	public String rangeAnalysis(
 			@RequestParam(name = "name", required = false, defaultValue = "/etc/passwd") String path) {
 		int j = 0;
+		// requires local range analysis to prove that the else branch is dead code
 		for (int i = 0; i < 100; i++) {
 			j += 2;
 		}
 		if (j >= 0) {
 			return "dead code";
+		} else {
+			return vuln(path);
 		}
-		return vuln(path);
+	}
+
+	@GetMapping("/rangeAnalysisInterProcedural")
+	public String rangeAnalysisInterProcedural(
+			@RequestParam(name = "name", required = false, defaultValue = "/etc/passwd") String path) {
+		int j = 0;
+		// requires range analysis across procedures to discover that the else branch is
+		// dead
+		for (int i = 0; i < 100; i++) {
+			j = addTwo(j);
+		}
+		if (j >= 0) {
+			return "dead code";
+		} else {
+			return vuln(path);
+		}
 	}
 
 	@GetMapping("/constant propagation - local")
 	public String constantPropagationLocal(
 			@RequestParam(name = "name", required = false, defaultValue = "/etc/passwd") String path) {
 		int x = 0;
+		// requires local analysis that the else branch is dead code
 		if (x == 0) {
 			return "dead code";
+		} else {
+			return vuln(path);
 		}
-		return vuln(path);
 	}
 
 	@GetMapping("/constant propagation - global")
 	public String constantPropagationGlobal(
 			@RequestParam(name = "name", required = false, defaultValue = "/etc/passwd") String path) {
+		// requires constant propagation across procedures to avoid false positive
 		boolean b = true;
 		return vulnIfFalse(path, b);
 	}
@@ -67,11 +92,13 @@ public class VulnerableController {
 	@GetMapping("/branchTracking")
 	public String branchTracking(
 			@RequestParam(name = "name", required = false, defaultValue = "/etc/passwd") String path, String what) {
-		// data flow analysis needs to discover that in this branch, conditional always is true, so
+		// data flow analysis needs to discover that in this branch, conditional always
+		// is true, so
 		// vulnerability does not hold
 		if (what == "bla") {
 			return vulnIfFalse(path, what == "bla");
+		} else {
+			return "nothing";
 		}
-		return "nothing";
 	}
 }
